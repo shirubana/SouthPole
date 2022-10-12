c45c8d7a0e6eed171ed1c6a116792f73d325c895
Silvana Ovaitt <silvana.ayala@nrel.gov>
2022-10-12 09:53:19 -0600

    cleanup and documentatino of WeatherFile creation

--- Journals/WeatherFile Creation.py
+++ Journals/WeatherFile Creation.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# # Weather File creation for South Pole NOAA Data
+# 
+# Data from: https://gml.noaa.gov/dv/data/index.php?site=SPO&category=Radiation
+# This converts all (already downloaded and saved) .dat for 1 year into a SAM-styled weather file
+# 
+# Data Readme: https://gml.noaa.gov/aftp/data/radiation/baseline/spo/README_BASELINE
+
 # In[1]:
 
 
@@ -12,6 +19,7 @@ import pvlib
 # In[2]:
 
 
+# PVlib SAM-output tool used is currently in a Pull Request to be implemented into next PVLib release.
 pvlib.__version__
 
 
@@ -24,7 +32,7 @@ weatherfolder = r'WeatherFileDats'
 # In[4]:
 
 
-# NOAO  -- pvlib / SAM
+# NOAA  -- pvlib / SAM
 # dw_solar = ghi
 # direct_n = dni
 # diffuse = dhi
@@ -71,22 +79,28 @@ df_all.set_index(df_all['timestamps'], inplace = True)
 # In[8]:
 
 
-df_all = df_all.iloc[:-60]
+#df_all = df_all.iloc[:-60]
 
 
 # In[9]:
 
 
-df_all.replace(to_replace = -9999.9, value =0 , inplace=True)# -9999.9]
+df_all
 
 
 # In[10]:
 
 
-#df_all['dhi'].replace(to_replace = -0.1, value =0 , inplace=True)# -9999.9]
+df_all.replace(to_replace = -9999.9, value =0 , inplace=True)# -9999.9]
 
 
 # In[11]:
+
+
+#df_all['dhi'].replace(to_replace = -0.1, value =0 , inplace=True)# -9999.9]
+
+
+# In[12]:
 
 
 df_all.loc[df_all['dni']<0,'dni']=0
@@ -94,13 +108,49 @@ df_all.loc[df_all['ghi']<0,'ghi']=0
 df_all.loc[df_all['dhi']<0,'dhi']=0
 
 
-# In[12]:
-
-
-df_all.replace(to_replace = -9999.9, value =0 , inplace=True)# -9999.9]-0.1
-
-
 # In[13]:
+
+
+# This functions are inside of PVlib, but they give 0 value on the 1st and last hours of the year, so this fixes that.
+
+def _averageSAMStyle(df, interval='60T', closed='left', label='left'):
+        ''' Averages subhourly data into hourly data in SAM's expected format.
+        '''
+        df = df.resample(interval, closed=closed, label=label).mean()
+        return df
+    
+def _fillYearSAMStyle(df, freq='60T'):
+    ''' Fills year
+    '''
+    # add zeros for the rest of the year
+    # add a timepoint at the end of the year
+    # apply correct TZ info (if applicable)
+    tzinfo = df.index.tzinfo
+    starttime = pd.to_datetime('%s-%s-%s %s:%s' % (df.index.year[0], 1, 1,
+                                                   0, 0)
+                               ).tz_localize(tzinfo)
+    endtime = pd.to_datetime('%s-%s-%s %s:%s' % (df.index.year[-1], 12, 31,
+                                                 23, 60-int(freq[:-1]))
+                             ).tz_localize(tzinfo)
+
+    df2 = _averageSAMStyle(df, freq)
+ #   df2.iloc[0] = 0  # set first datapt to zero to forward fill w zeros
+ #   df2.iloc[-1] = 0  # set last datapt to zero to forward fill w zeros
+   # df2.loc[starttime] = 0
+   # df2.loc[endtime] = 0
+    df2 = df2.resample(freq).ffill()
+    return df2
+
+
+# In[14]:
+
+
+data = df_all.copy()
+data = _fillYearSAMStyle(data)
+data
+
+
+# In[15]:
 
 
 metdata = {'latitude':  -89.98,  # From the NOAA metdata
@@ -110,14 +160,8 @@ metdata = {'latitude':  -89.98,  # From the NOAA metdata
             'TZ':-3}
 
 
-# In[14]:
+# In[16]:
 
 
-pvlib.iotools.saveSAM_WeatherFile(df_all, metdata, savefile='SAM_SP_WeatherFile.csv')
-
-
-# In[ ]:
-
-
-
+pvlib.iotools.saveSAM_WeatherFile(data, metdata, savefile='SAM_SP_WeatherFile.csv', standardSAM=True)
 
